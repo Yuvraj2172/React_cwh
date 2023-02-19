40236c3bb8dcfe17d156d96d2ca18e96ba3440e5
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-19 18:30:35 +0530

    Init

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+let name = "Yuvraj";
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Built by {name}</li>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    </>
+  );
+}
+
+export default App;
